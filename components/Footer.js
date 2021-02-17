d08199d05e2369ab365e82c5cfcf094ac7d816d1
Cameron Jenkinson <camjenkinson@gmail.com>
2021-02-17 11:28:55 +0000

    Fix footer

--- components/Footer.js
+++ components/Footer.js
@@ -5,59 +5,37 @@ import { Flex, Link, IconButton } from '@chakra-ui/core';
 const Footer = () => (
   <Flex align="center" mb={4} direction="column">
     <div>
-      <Link href="https://instagram.com/selfsourced" title="Instagram" isExternal>
-        <IconButton
-          aria-label="Instagram"
-          icon="instagram"
-          size="lg"
-          color="gray.500"
-          variant="ghost"
-        />
-      </Link>
-      <Link
-        href="https://www.youtube.com/channel/UCZMli3czZnd1uoc1ShTouQw"
-        title="YouTube"
-        isExternal
-      >
-        <IconButton
-          aria-label="YouTube"
-          icon="youtube"
-          size="lg"
-          color="gray.500"
-          variant="ghost"
-        />
-      </Link>
-      <Link href="mailto:hello@selfsourcedman.com" title="Email" isExternal>
-        <IconButton
-          aria-label="Email"
-          icon="mail"
-          size="lg"
-          color="gray.500"
-          variant="ghost"
-        />
-      </Link>
-    </div>
-    <div>
-      <NextLink href="/about" passHref>
+      <NextLink href="https://www.youtube.com/channel/UC7zYDihxb-Nlu9lOgMEJhjA" passHref>
         <Link
           fontSize="sm"
           color="gray.500"
           minWidth="100px"
           mr={2}
-          title="Subscribe"
+          title="YouTube"
         >
-          /about
+          /youtube
         </Link>
       </NextLink>
-      <NextLink href="/blog" passHref>
+      <NextLink href="/https://instagram.com/selfsourced" passHref>
         <Link
           fontSize="sm"
           color="gray.500"
           minWidth="100px"
           mr={2}
-          title="Blog"
+          title="Instagram"
         >
-          /blog
+          /instagram
+        </Link>
+      </NextLink>
+      <NextLink href="mailto:cameron@selfsourcedman.com" passHref>
+        <Link
+          fontSize="sm"
+          color="gray.500"
+          minWidth="100px"
+          mr={2}
+          title="Email"
+        >
+          /email
         </Link>
       </NextLink>
     </div>
